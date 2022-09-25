8127fc18c4e735ae94a3c720287eba431520313e
Raisa Tarase <raisa.tarase@gmail.com>
2022-09-25 16:23:17 +0200

    node packages installed

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for request
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+  // res.send('<p>home page</p>');
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+
+  // res.send('<p>about page</p>');
+
+  res.sendFile('./views/about.html', { root: __dirname });
+
+});
