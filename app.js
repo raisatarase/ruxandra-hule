e745dc26d67e546b76cc8d0df7e2440d12a30815
Raisa Tarase <raisa.tarase@gmail.com>
2022-09-25 22:42:28 +0200

    routing created

--- app.js
+++ app.js
@@ -7,15 +7,28 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-
-  // res.send('<p>home page</p>');
   res.sendFile('./views/index.html', { root: __dirname });
 });
 
 app.get('/about', (req, res) => {
-
-  // res.send('<p>about page</p>');
-
   res.sendFile('./views/about.html', { root: __dirname });
+});
 
+app.get('/gallery', (req, res) => {
+  res.sendFile('./views/gallery.html', { root: __dirname });
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile('./views/contact.html', { root: __dirname });
+});
+
+// redirects
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+
+// 404 page goes to the bottom
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
